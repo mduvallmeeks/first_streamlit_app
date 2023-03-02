3f43a7997ea3e3e766f723012da729744a07b369
McKenzie DuVall-Meeks <mduvallmeeks@gmail.com>
2023-03-02 17:21:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # writes formatted version to page
 streamlit.dataframe(fruityvice_normalized)
-
+streamlit.stop()
 
 import snowflake.connector
 
