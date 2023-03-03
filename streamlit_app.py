920a8bbc7cf6fbb771427e231c9d11e06434ca1c
McKenzie DuVall-Meeks <mduvallmeeks@gmail.com>
2023-03-02 18:02:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,10 +38,11 @@ try:
 except URLerror as e:
   streamlit.error()
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
+    my_cur.close()
     return my_cur.fetchall()
 
 if streamlit.button('Get Fruit Load List'):
@@ -52,6 +53,7 @@ if streamlit.button('Get Fruit Load List'):
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
+    my_cur.close()
     return "Thanks for adding" + new_fruit
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
